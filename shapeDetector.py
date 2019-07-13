c280e7833db71d969b739002c1466035bcf1287f
Sanghoon <noohgnas@gmail.com>
2019-07-13 21:12:21 +0900

    rostopic 전송 하도록 수정
    그런데... 3각형 그림을 보여줘도 4각형으로 인식되는 경우가 있다. 응?

--- shapeDetector.py
+++ shapeDetector.py
@@ -4,10 +4,13 @@ from std_msgs.msg import String
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import cv2
+import cv2 as cv
 import numpy as np
 import math
 
+pub = rospy.Publisher('robotmaker/shape_arm', String, queue_size=10)
+rospy.init_node('detector', anonymous=True)
+rate = rospy.Rate(100) # 10hz
 
 def setLabel(image, str, contour):
         (text_width, text_height), baseline = cv.getTextSize(str, cv.FONT_HERSHEY_SIMPLEX, 0.7, 1)
@@ -26,40 +29,44 @@ def getContours():
         for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
                 img_color = frame.array
                 img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
+                cv.imshow("Frame", img_gray)
+                key = cv.waitKey(1) & 0xFF
+                rawCapture.truncate(0)
+                if key == ord("q"):
+                        break
+
                 ret, img_binary = cv.threshold(img_gray, 127, 255, cv.THRESH_BINARY_INV|cv.THRESH_OTSU)
                 _, contours, hierarchy = cv.findContours(img_binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
                 for cnt in contours:
                         size = len(cnt)
-                        print(size)
+#                       print(size)
                         epsilon = 0.005 * cv.arcLength(cnt, True)
                         approx = cv.approxPolyDP(cnt, epsilon, True)
 
                         size = len(approx)
-                        print(size)
+#                       print(size)
 
                         cv.line(img_color, tuple(approx[0][0]), tuple(approx[size-1][0]), (0, 255, 0), 3)
                         for k in range(size-1):
                                 cv.line(img_color, tuple(approx[k][0]), tuple(approx[k+1][0]), (0, 255, 0), 3)
 
                         if cv.isContourConvex(approx):
-                                shapeDetector(cnt)
+                                print(size)
+                                shapeDetector(size)
                         else:
                                 setLabel(img_color, str(size), cnt)
 
 
-def shapeDetector(cnt):
-    pub = rospy.Publisher('robotmaker/shape_arm', String, queue_size=10)
-    rospy.init_node('detector', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "contour: %s" % str(cnt)
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+def shapeDetector(size):
+        if not rospy.is_shutdown():
+                send_str = "s:%s" % str(size)
+                rospy.loginfo(send_str)
+                pub.publish(send_str)
+                rate.sleep()
 
 if __name__ == '__main__':
-    try:
-        getContours()
-    except rospy.ROSInterruptException:
-        pass
+        try:
+                getContours()
+        except rospy.ROSInterruptException:
+                pass
